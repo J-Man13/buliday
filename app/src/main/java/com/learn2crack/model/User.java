b9235250d5d695bc8f83d1bf7135b30625a0175b
Jafar Jafarov <cafar.9r4@gmail.com>
2017-06-07 13:34:35 +0400

    Project files commit. Registration/Login

---
+++ app/src/main/java/com/learn2crack/model/User.java
@@ -0,0 +1,87 @@
+package com.learn2crack.model;
+
+
+public class User {
+
+    private String name;
+    private String surname;
+    private String email;
+    private String gender;
+    private String birthdate;
+    private String password;
+    private String created_at;
+    private String newPassword;
+    private String token;
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
